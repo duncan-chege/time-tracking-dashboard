aa3729146cf0d977fac7f69dd04aedd1ced97049
viisualworks <dshege4@gmail.com>
2024-11-15 10:21:52 +0300

    configure project colors and font type

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "blue": "hsl(246, 80%, 60%)",
+        "very-dark-blue": "hsl(226, 43%, 10%)",
+        "dark-blue": "hsl(235, 46%, 20%)",
+        "desaturated-blue": "hsl(235, 45%, 61%)",
+        "pale-blue": "hsl(236, 100%, 87%)",
+        "light-red-work": "hsl(15, 100%, 70%)",
+        "soft-blue": "hsl(195, 74%, 62%)",
+        "light-red-study": "hsl(348, 100%, 68%)",
+        "lime-green": "hsl(145, 58%, 55%)",
+        "violet": "hsl(264, 64%, 52%)",
+        "soft-orange": "hsl(43, 84%, 65%)",
       },
     },
   },
