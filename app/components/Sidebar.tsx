bc19a46f020013862f17622f1bb4b0160fc56d49
viisualworks <dshege4@gmail.com>
2024-11-19 12:52:46 +0300

    complete creating the sidebar component

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,7 +1,23 @@
+// Finish project by Thursday, 21st November
+
+import Image from "next/image"
+import profilePic from "../../public/images/image-jeremy.png";
+
 export default function Sidebar(){
-    return (
-        <div className="bg-violet">
-            Sidebar content here
+return (
+    <div className="w-1/5 bg-dark-blue rounded-xl">
+        <div className="bg-blue text-white rounded-xl p-5 font-light">
+            <Image className="border-white border-2 rounded-full w-1/4" src={profilePic} alt="" />
+            <p className="opacity-70 text-sm mt-12">Report For</p>
+            <h3 className="text-4xl mb-12">Jeremy<br /> Robson</h3>
         </div>
-    )
+        <div className="text-desaturated-blue p-5">
+            <ul>
+                <li className="py-2">Daily</li>
+                <li className="py-2">Weekly</li>
+                <li className="py-2">Monthly</li>
+            </ul>
+        </div>
+    </div>
+)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,9 @@ import Sidebar from "./components/Sidebar";
 
 export default function Page() {
     return (
-        <>
+        <div className="bg-very-dark-blue min-h-screen flex flex-col justify-center">
             <Sidebar />
-        </>
+            <></>
+        </div>
     )
 }
