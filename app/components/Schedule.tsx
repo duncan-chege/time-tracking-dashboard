b761e2ffe93eb0399161b6f808eeef03458a10b3
viisualworks <dshege4@gmail.com>
2024-11-20 12:57:18 +0300

    add hover to schedule-blocs

--- app/components/Schedule.tsx
+++ app/components/Schedule.tsx
@@ -13,7 +13,7 @@ const Schedule = () => {
     <div className="md:w-3/4 grid lg:grid-cols-3 md:grid-cols-2 gap-8">
         <div className="bg-light-red-work rounded-xl pt-12 pb-0 relative z-50">
             <Image className="absolute right-8 top-0 -z-10" src={workIcon} alt="" />
-            <div className="rounded-xl p-10 text-white bg-dark-blue">
+            <div className="rounded-xl p-10 text-white cursor-pointer bg-dark-blue hover:saturate-200">
                 <div className="flex justify-between items-center pb-6">
                     <p className="text-xl">Work</p>
                     <Image className="h-auto" src={ellipses} alt="" />
@@ -25,7 +25,7 @@ const Schedule = () => {
 
         <div className="bg-soft-blue rounded-xl pt-12 relative z-50">
             <Image className="absolute right-8 top-0 -z-10" src={playIcon} alt="" />
-            <div className="rounded-xl p-10 text-white bg-dark-blue">
+            <div className="rounded-xl p-10 text-white cursor-pointer bg-dark-blue hover:saturate-200">
                 <div className="flex justify-between items-center pb-6">
                     <p className="text-xl">Play</p>
                     <Image className="h-auto" src={ellipses} alt="" />
@@ -37,7 +37,7 @@ const Schedule = () => {
 
         <div className="bg-light-red-study rounded-xl pt-12 relative z-50">
             <Image className="absolute right-8 top-0 -z-10" src={studyIcon} alt="" />
-            <div className="rounded-xl p-10 text-white bg-dark-blue">
+            <div className="rounded-xl p-10 text-white cursor-pointer bg-dark-blue hover:saturate-200">
                 <div className="flex justify-between items-center pb-6">
                     <p className="text-xl">Study</p>
                     <Image className="h-auto" src={ellipses} alt="" />
@@ -49,7 +49,7 @@ const Schedule = () => {
 
         <div className="bg-lime-green rounded-xl pt-12 relative z-50">
             <Image className="absolute right-8 top-0 -z-10" src={exerciseIcon} alt="" />
-            <div className="rounded-xl p-10 text-white bg-dark-blue">
+            <div className="rounded-xl p-10 text-white cursor-pointer bg-dark-blue hover:saturate-200">
                 <div className="flex justify-between items-center pb-6">
                     <p className="text-xl">Work</p>
                     <Image className="h-auto" src={ellipses} alt="" />
@@ -61,7 +61,7 @@ const Schedule = () => {
 
         <div className="bg-violet rounded-xl  pt-12 relative z-50">
             <Image className="absolute right-8 top-0 -z-10" src={socialIcon} alt="" />
-            <div className="rounded-xl p-10 text-white bg-dark-blue">
+            <div className="rounded-xl p-10 text-white cursor-pointer bg-dark-blue hover:saturate-200">
                 <div className="flex justify-between items-center pb-6">
                     <p className="text-xl">Work</p>
                     <Image className="h-auto" src={ellipses} alt="" />
@@ -73,7 +73,7 @@ const Schedule = () => {
 
         <div className="bg-soft-orange rounded-xl  pt-12 relative z-50">
             <Image className="absolute right-8 top-0 -z-10" src={careIcon} alt="" />
-            <div className="rounded-xl p-10 text-white bg-dark-blue">
+            <div className="rounded-xl p-10 text-white cursor-pointer bg-dark-blue hover:saturate-200">
                 <div className="flex justify-between items-center pb-6">
                     <p className="text-xl">Work</p>
                     <Image className="h-auto" src={ellipses} alt="" />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        "blue": "hsl(246, 80%, 60%)",
+        blue: "hsl(246, 80%, 60%)",
         "very-dark-blue": "hsl(226, 43%, 10%)",
         "dark-blue": "hsl(235, 46%, 20%)",
         "desaturated-blue": "hsl(235, 45%, 61%)",
@@ -18,7 +18,7 @@ export default {
         "soft-blue": "hsl(195, 74%, 62%)",
         "light-red-study": "hsl(348, 100%, 68%)",
         "lime-green": "hsl(145, 58%, 55%)",
-        "violet": "hsl(264, 64%, 52%)",
+        violet: "hsl(264, 64%, 52%)",
         "soft-orange": "hsl(43, 84%, 65%)",
       },
     },
