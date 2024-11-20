5b1f58092e831bcfb105178438767e1f003fdf3a
viisualworks <dshege4@gmail.com>
2024-11-20 11:51:48 +0300

    complete responsiveness of the layout

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -5,17 +5,19 @@ import profilePic from "../../public/images/image-jeremy.png";
 
 export default function Sidebar(){
 return (
-    <div className="w-1/4 bg-dark-blue rounded-xl">
-        <div className="bg-blue text-white rounded-xl p-8 font-light">
-            <Image className="border-white border-2 rounded-full w-1/4" src={profilePic} alt="" />
-            <p className="opacity-70 text-sm mt-12">Report For</p>
-            <h3 className="text-4xl mb-12">Jeremy<br /> Robson</h3>
+    <div className="md:w-1/4 bg-dark-blue rounded-xl">
+        <div className="bg-blue text-white rounded-xl lg:p-8 p-6 font-light flex items-center md:block">
+            <Image className="border-white border-2 rounded-full w-1/4 md:w-1/2" src={profilePic} alt="" />
+            <div className="md:my-12 md:ml-0 ml-4">
+                <p className="opacity-70 ">Report For</p>
+                <h3 className="lg:text-5xl md:text-3xl text-2xl">Jeremy Robson</h3>
+            </div>
         </div>
-        <div className="text-desaturated-blue p-8">
+        <div className="text-desaturated-blue lg:p-8 p-6">
             <ul>
-                <li className="py-2">Daily</li>
-                <li className="py-2">Weekly</li>
-                <li className="py-2">Monthly</li>
+                <li className="py-2 text-lg">Daily</li>
+                <li className="py-2 text-lg">Weekly</li>
+                <li className="py-2 text-lg">Monthly</li>
             </ul>
         </div>
     </div>
